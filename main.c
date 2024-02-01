3194fbb1ef9b6cc21eaab56fb74e208e8c6e848f
as-labs <a.alshaiji@protonmail.com>
2024-02-01 16:42:29 +0300

    feat(main.c): add initial Vulkan setup and instance creation
    This commit adds the initial setup for a Vulkan application in the main.c file. It includes the necessary headers, initializes GLFW, and creates a Vulkan instance. The instance is created with the required application information, including the application name, engine name, and API version. The commit also enables the validation layer for debugging purposes.

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+
+
+#include "vulkan/vulkan.h"
+#include "GLFW/glfw3.h"
+#define GLFW_INCLUDE_VULKAN
+
+
+int main(int argc, char **argv) {
+	glfwInit();
+
+	VkApplicationInfo app_info;
+
+	app_info.sType=VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	app_info.pNext=NULL;
+	char app_name[VK_MAX_EXTENSION_NAME_SIZE];
+	strcpy(app_name, "vulkan_project");
+	app_info.pApplicationName=app_name;
+	app_info.applicationVersion=VK_MAKE_VERSION(0,0,1);
+	char app_engine_name[VK_MAX_EXTENSION_NAME_SIZE];
+	strcpy(app_engine_name,"vulkan_engine");
+	app_info.pEngineName=app_engine_name;
+	app_info.engineVersion=VK_MAKE_VERSION(0,0,1);
+	app_info.apiVersion=VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo inst_cre_info;
+
+	inst_cre_info.sType=VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	inst_cre_info.pNext=NULL;
+	inst_cre_info.flags=0;
+	inst_cre_info.pApplicationInfo=&app_info;
+	uint32_t inst_layer_count=1;
+	inst_cre_info.enabledLayerCount = inst_layer_count;
+	char pp_inst_layers[inst_layer_count] [VK_MAX_EXTENSION_NAME_SIZE];
+	strcpy(pp_inst_layers[0],"VK_LAYER_KHRONOS_validation");
+	char *pp_inst_layers_names[inst_layer_count];
+
+	for(uint32_t i=0;i<inst_layer_count;i++) {
+		pp_inst_layers_names[i] = pp_inst_layers[i];
+	}
+
+	inst_cre_info.ppEnabledLayerNames = (const char * const *)pp_inst_layers_names;
+
+	uint32_t inst_ext_count=0;
+	const char * const *pp_inst_ext_names=glfwGetRequiredInstanceExtensions(&inst_ext_count);
+
+	VkInstance inst;
+
+	vkCreateInstance(&inst_cre_info,NULL,&inst);
+	printf("instance created.\n");
+}
